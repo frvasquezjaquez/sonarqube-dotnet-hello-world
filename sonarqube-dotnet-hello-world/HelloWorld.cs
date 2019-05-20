9e97046a26fae9d99bbd8c3ce368dbac923d4ab5
Francisco J. Vásquez <frvasquezjaquez@gmail.com>
2019-05-20 13:13:52 -0400

    Repo Init

---
+++ sonarqube-dotnet-hello-world/HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sonarqube_dotnet_hello_world
+{
+    public class HelloWorld
+    {
+        int i = 0;
+    }
+}
